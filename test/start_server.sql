90fb21cc9fc39ddb3d8925ee26a1739ce3628b33
Dirk <42852469+dirkgermany@users.noreply.github.com>
2026-02-13 11:12:43 +0100

    Change server creation and execution to LILAM

--- test/start_server.sql
+++ test/start_server.sql
@@ -5,10 +5,10 @@ DECLARE
     v_pipe_name VARCHAR2(100);
     v_processId number;
 BEGIN
-    v_pipe_name := LILA.CREATE_SERVER('<password>');
+    v_pipe_name := LILAM.CREATE_SERVER('<password>');
     DBMS_OUTPUT.PUT_LINE('Server gestartet auf Pipe: ' || v_pipe_name);
 END;
 
 -- Lesser powerfull environments (e.g. Test-PC)
 -- Password will be needed later for shutdown
-exec lila.start_server('LILA_P3', '<password>');
+exec lilam.start_server('LILAM_P3', '<password>');
