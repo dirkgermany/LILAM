d97826b8ebe29fa0f5da2d5ea870a5f612bbc6a9
Dirk <42852469+dirkgermany@users.noreply.github.com>
2026-03-26 21:50:15 +0100

    Update SQL query to use new table names and calculations

--- test/observe_progress.sql
+++ test/observe_progress.sql
@@ -1,11 +1,15 @@
-select distinct proc.id proc_id, proc.process_name process, proc.process_start, proc.proc_steps_todo, proc.proc_steps_done proc_steps_done, detail.action_count,
-        proc.proc_steps_done / proc.proc_steps_todo * 100 percent_of_work,
+select distinct proc.id proc_id, proc.process_name process, proc.process_start, proc.steps_todo, proc.steps_done proc_steps_done, detail.action_count,
+        proc.steps_done / proc.steps_todo * 100 percent_of_work,
+
+        round( (TO_NUMBER(TO_CHAR(proc.process_start, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. ''') - 
+         TO_NUMBER(TO_CHAR(proc.last_update, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. '''))*-1, 2)  bla,        
+        
         round((TO_NUMBER(TO_CHAR(proc.process_start, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. ''') - 
          TO_NUMBER(TO_CHAR(proc.last_update, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. ''')) /-60, 2) min_work
-from remote_log proc
+from lilamtest_proc proc
 join (
     select process_id, max(action_count) action_count
-    from remote_log_mon
+    from lilamtest_mon mon
     group by process_id
 ) detail
     on detail.process_id = proc.id
